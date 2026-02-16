9da86ccd599809fc602e2c936a37229c9586ccfa
Erich Vonderhorst <109879579+evonderhorst@users.noreply.github.com>
2026-02-16 18:22:55 -0500

    Add files via upload

--- data.py
+++ data.py
@@ -1,43 +1,35 @@
 import matplotlib
 import numpy
 import scipy
+import csv
 
-def extractData(fileName):
-    file = open(fileName, encoding='utf-8-sig')
-    data = []
-    colHeaders = file.readline().rstrip().split(',')
-    data.append(colHeaders)
-    file.seek(0)
-    for line in file:
-        data.append((file.readline().rstrip()).split(','))
-    return data
- 
-data = extractData("ammonia.csv")
+with open("chlorophyl.csv", encoding='utf-8-sig') as file:
+    data=[]
+    reader = csv.reader(file)
+    for row in reader:
+        data.append(row)
 
-xlist = []
-ylist = []
-for row in data[1:]:
-    print(row)
-    if ( row[9] == '' ):
-        continue
-    ylist.append((float)(row[9]))     
-    xlist.append((int)(row[2]))
-
-print(xlist)
-print(ylist)
-
-x = numpy.array(xlist)
-y = numpy.array(ylist)
-
-slope, intercept, r, p, std_err = scipy.stats.linregress(x, y)
-
-def point(x):
-  return slope * x + intercept
-
-linreg = list(map(point, x))
-
-matplotlib.pyplot.scatter(x, y)
-matplotlib.pyplot.plot(x, linreg)
-
-#matplotlib.pyplot.plot(x, y, linestyle = "solid")
-matplotlib.pyplot.show()
+    xlist = []
+    ylist = []
+    for row in data[1:538]:
+        print(row)
+        if ( row[9] == '' ):
+            continue
+        ylist.append((float)(row[9]))     
+        xlist.append((int)(row[2]))
+    
+    print(xlist)
+    print(ylist)
+    
+    x = numpy.array(xlist)
+    y = numpy.array(ylist)
+    
+    slope, intercept, r, p, std_err = scipy.stats.linregress(x, y)
+    
+    def ypoint(x):
+      return slope * x + intercept
+    
+    linreg = list(map(ypoint, x))
+    
+    matplotlib.pyplot.scatter(x, y, color="blue")
+    matplotlib.pyplot.plot(x, linreg, marker = "D", linestyle = "solid", color="darkblue")
