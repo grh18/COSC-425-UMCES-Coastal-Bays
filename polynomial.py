982246cf5922435101bbf79ab4fe836e51cf9aee
Erich Vonderhorst <109879579+evonderhorst@users.noreply.github.com>
2026-04-05 22:01:58 -0400

    Add files via upload
    Completed implementation

--- polynomial.py
+++ polynomial.py
@@ -1,22 +1,27 @@
 import matplotlib
+import matplotlib.patheffects
 import numpy
 import csv
-from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
 from sklearn.preprocessing import PolynomialFeatures
-
-FILENAME = "chlorophyll.csv"
+  
+FILENAME = "nitrate.csv"
+FILEPATH = "nutrients/cluster1/"
+DATACOL = 9
+STATION = "I"
+DEGREE = 3
 YEARFIRST = 2009
 YEARLAST = 2021
-YLABEL = "Chlorophyll a (Grams per Litre)"
-DOTCOLOR = "green"
-LINECOLOR = "darkgreen"
+YLABEL = "NH4 (Micromolars)"
+DOTCOLOR = "magenta"
+LINECOLOR = "purple"
 LINETYPE = "solid"
 MARK = "d"
-LEFTSCALE = numpy.arange(0, 11, 2)
-BOTTOMSCALE = numpy.arange(YEARFIRST,YEARLAST + 1, 2)
-CFACTOR = 1
+LEFTSCALE = numpy.arange(0, 2.1, .5)
+BOTTOMSCALE = numpy.arange(2008, 2023, 2)
+CFACTOR = 1.0
 
-with open(FILENAME, encoding='utf-8-sig') as file:
+with open(FILEPATH + FILENAME, encoding='utf-8-sig') as file:
     data=[]
     reader = csv.reader(file)
     for row in reader:
@@ -49,37 +54,38 @@ with open(FILENAME, encoding='utf-8-sig') as file:
             if ((int)(row[2]) not in years):
                 years.append((int)(row[2]))
             
-    x = numpy.array(xlist).reshape(-1,1)
-    y = numpy.array(ylist)
-    
-    pmodel = PolynomialFeatures(degree=3, include_bias=False)
-    curvex = pmodel.fit_transform(x)
-    lmodel = LinearRegression()
-    lmodel.fit(curvex, y)
-    curvey = lmodel.predict(curvex)
+    x = numpy.array(xlist).reshape(-1, 1)
+    y = numpy.array(ylist).reshape(-1, 1)
+    poly = PolynomialFeatures(degree=DEGREE, include_bias=False)
+    curvex = poly.fit_transform(x)
+    curvex = sm.add_constant(curvex)
+    model = sm.OLS(y, curvex).fit(method = 'qr')
+    curvey = model.predict(curvex)
         
     nutrient = (FILENAME.split('.'))[0]
     nutrient = nutrient[0].upper() + nutrient[1:]
-    print("\n" + nutrient)
-    print("-" * len(nutrient))
-    #print("Slope: " + (str)(slope))
-    #print("Intercept: " + (str)(intercept))
-    print("R-Squared: " + (str)(lmodel.score(curvex, y)))
-    #print("Standard Error: " + (str)(std_err))
-    print("\n")
-        
+    
+    print(model.summary())
+    
+    rs = model.rsquared_adj
+    p = model.f_pvalue
     fig, axes = matplotlib.pyplot.subplots()
-    matplotlib.pyplot.plot(x,curvey, marker = "", linestyle = LINETYPE, color=LINECOLOR, label = nutrient)
-    matplotlib.pyplot.scatter(x, y, color=DOTCOLOR, marker = MARK)
+    fig.text(.1,.89, f"R\u00b2 = {rs: .5f}     P = {p: .5f}     Degree = {DEGREE}", color = DOTCOLOR).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
+    axes.scatter(x, y, color=DOTCOLOR, marker = MARK, label = nutrient)
+    axes.plot(x,curvey, marker = "", linestyle = LINETYPE, color=LINECOLOR, label = nutrient + " Trend")
     axes.set_yticks(LEFTSCALE)
     axes.set_xticks(BOTTOMSCALE)
+    matplotlib.pyplot.xlabel("Year")
+    matplotlib.pyplot.ylabel(YLABEL, color=DOTCOLOR).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
     
     nutrient = (FILENAME.split('.'))[0]
     nutrient = nutrient[0].upper() + nutrient[1:]
-    title = nutrient
+    title = nutrient + " (" + (str)(years[0]) + " - " + (str)(years[-1]) + ") Station " + STATION
     matplotlib.pyplot.xlabel("Year")
     matplotlib.pyplot.title(title)
     matplotlib.pyplot.ylabel(YLABEL)
-    matplotlib.pyplot.tight_layout(pad=3)
-    matplotlib.pyplot.legend(loc = "upper left")
+    fig.text(.05,.75, STATION, fontsize=30, ha="center")
+    matplotlib.pyplot.title(title)
+    matplotlib.pyplot.tight_layout(pad=4)
+    fig.legend(ncol=4, loc = "upper left")
     matplotlib.pyplot.show()
