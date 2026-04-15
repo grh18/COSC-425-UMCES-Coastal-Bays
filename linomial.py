0ebc0a95a4275bedcbae26becd5cd2f8799c72e8
Erich Vonderhorst <109879579+evonderhorst@users.noreply.github.com>
2026-04-15 12:17:55 -0400

    Add files via upload
    Updated to include weighted least squares for heteroschedastic data

--- linomial.py
+++ linomial.py
@@ -3,6 +3,7 @@ import matplotlib.patheffects
 import numpy
 import scipy
 import statsmodels.api as sm
+from statsmodels.stats.diagnostic import het_breuschpagan
 from sklearn.preprocessing import PolynomialFeatures
 import csv
 
@@ -10,15 +11,17 @@ FILENAME = "phosphate.csv"
 FILEPATH = "nutrients/Cluster1/"
 DATACOL = 9
 YEARCOL = 2
-STATION = "I"
+SEGMENT = "I"
 YEARFIRST = 1995
 YEARLAST = 2022
-YLABEL = "micrograms per liter"
+YLABEL = "micromolars"
 DOTCOLOR = "orange"
 LINECOLOR = "red"
 LINETYPE = "solid"
-LINECOLOR2 = "magenta"
+LINECOLOR2 = "yellow"
 LINETYPE2 = "dotted"
+LINECOLOR3 = "magenta"
+LINETYPE3 = "solid"
 MARK = "d"
 LEFTSCALE = numpy.arange(0, 1.1, .25)
 BOTTOMSCALE = numpy.arange(1994, 2025, 2)
@@ -61,46 +64,59 @@ def plot (file, cfactor):
     x = numpy.array(xlist)
     y = numpy.array(ylist)
     
-    slope, intercept, r, p, std_err = scipy.stats.linregress(x, y)
-    
-    def yline(x):
-        return slope * x + intercept
-    linreg = list(map(yline, x))
-    
-    return  x, y, r, p, std_err, linreg, years
+    return  x, y, years
 
 fig, axes = matplotlib.pyplot.subplots()
 
 with open(FILEPATH + FILENAME, encoding='utf-8-sig') as file:
-    x, y, r, p, std_err, linreg, years = plot(file, CFACTOR)
+    x, y, years = plot(file, CFACTOR)
+    axes.scatter(x, y, color=DOTCOLOR, marker = MARK)
+    
+    xc = sm.add_constant(x)
+    model = sm.OLS(y, xc).fit()
+    lm, lm_pvalue, fvalue, f_pvalue = het_breuschpagan(model.resid_pearson, xc)
+    if (lm_pvalue > .05):
+        curvey = model.predict(xc)
+        axes.plot(x, curvey, marker = "", linestyle = LINETYPE, color=LINECOLOR, label = "OLS Linear Trend")
+        rsq = model.rsquared_adj
+        pval = model.f_pvalue
+        fig.text(.2,.89, f"P = {pval:.5f}", color = LINECOLOR).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
+        fig.text(.2,.85, f"R\u00b2 = {rsq: .5f}", color = LINECOLOR).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
+    else:    
+        curvex = sm.add_constant(x)
+        w = sm.OLS(y, curvex).fit().resid_pearson
+        model = sm.WLS(y, curvex, weights = 1.0 / w**2).fit()
+        rsq = model.rsquared_adj
+        pval = model.f_pvalue
+        curvey = model.predict(curvex)
+        axes.plot(x, curvey, marker = "", linestyle = LINETYPE2, color = LINECOLOR2, label = "WLS Linear trend")
+        fig.text(0.2,.89, f"P = {pval:.5f}", color = LINECOLOR2).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
+        fig.text(0.2,.85, f"R\u00b2 = {rsq: .5f}", color = LINECOLOR2).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
+        
+    x2 = numpy.array(x).reshape(-1, 1)
+    y2 = numpy.array(y).reshape(-1, 1)
+    poly = PolynomialFeatures(degree=DEGREE, include_bias=True)
+    curvex = poly.fit_transform(x2)
+    curvex = sm.add_constant(curvex)
+    model = sm.OLS(y2, curvex).fit(method = 'qr')
+    curvey = model.predict(curvex)
+    axes.plot(x, curvey, marker = "", linestyle = LINETYPE3, color = LINECOLOR3, label = f"Degree {DEGREE} Polynomial Trend")   
+    rsq = model.rsquared_adj
+    pval = model.f_pvalue
+    fig.text(.5,.89, f"P = {pval:.5f} ", color = LINECOLOR3).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])    
+    fig.text(.5,.85, f"R\u00b2 = {rsq: .5f}", color = LINECOLOR3).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
     
     nutrient = (FILENAME.split('.'))[0]
     nutrient = nutrient[0].upper() + nutrient[1:]
-    
-    title = nutrient + " (" + (str)(years[0]) + " - " + (str)(years[-1]) + ") Station " + STATION 
-    
-    fig.text(.1,.89, f"P = {p:.5f}    R\u00b2 = {r*r: .5f}", color = LINECOLOR).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
-    axes.scatter(x, y, color=DOTCOLOR, marker = MARK)
-    axes.plot(x, linreg, marker = "", linestyle = LINETYPE, color=LINECOLOR, label = "Linear Trend")
+    title = nutrient + " (" + (str)(years[0]) + " - " + (str)(years[-1]) + ") Segment " + SEGMENT 
     matplotlib.pyplot.xlabel("Year")
     matplotlib.pyplot.ylabel(YLABEL, color=DOTCOLOR).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
-    x = numpy.array(x).reshape(-1, 1)
-    y = numpy.array(y).reshape(-1, 1)
-    poly = PolynomialFeatures(degree=DEGREE, include_bias=True)
-    curvex = poly.fit_transform(x)
-    curvex = sm.add_constant(curvex)
-    model = sm.OLS(y, curvex).fit(method = 'qr')
-    curvey = model.predict(curvex)
-    axes.plot(x, curvey, marker = "", linestyle = LINETYPE2, color = LINECOLOR2, label = f"Degree {DEGREE} Polynomial Trend")       
     axes.set_yticks(LEFTSCALE)
     axes.set_xticks(BOTTOMSCALE)
     matplotlib.pyplot.xticks(rotation=60)
-    rsq = model.rsquared_adj
-    pval = model.f_pvalue
-    fig.text(.6,.89, f"P = {pval:.5f}    R\u00b2 = {rsq: .5f}", color = LINECOLOR2).set_path_effects([matplotlib.patheffects.withSimplePatchShadow(offset=(.6, -.6), shadow_rgbFace="black", alpha = .5, rho = 0)])
 
-fig.text(.05,.75, STATION, fontsize=30, ha="center")
+fig.text(.05,.75, SEGMENT, fontsize=30, ha="center")
 matplotlib.pyplot.title(title)
 matplotlib.pyplot.tight_layout(pad=5)
-fig.legend(ncol=2, loc = "upper center")
+fig.legend(ncol=3, loc = "upper center")
 matplotlib.pyplot.show()
